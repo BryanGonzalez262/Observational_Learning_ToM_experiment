c9345f5949a17a86f489c8d9b345636f9e162e9f
Bryan <bsg262@gmail.com>
2020-08-09 13:57:30 -0400

    updated cross validation

--- Code/utils.py
+++ Code/utils.py
@@ -16,65 +16,18 @@ def compute_aic(n_obs, sse, n_par):
     return aic
 
 
-def soft_max(x, tau):
+def old_soft_max(x, tau):
     """Compute softmax values for each sets of scores in x."""
     e_x = np.exp(np.array(x) / tau)
     return e_x / e_x.sum(axis=0)
 
 
-def old_softmax(x, temperature):
-    EPSILON = 10e-16  # to avoid taking the log of zero
-    # print(preds)
-    x = (np.array(x+ EPSILON)).astype('float64')
-    preds = np.log(x) / temperature
-    # print(preds)
-    exp_preds = np.exp(preds)
-    # print(exp_preds)
-    preds = exp_preds / np.sum(exp_preds)
-    # print(preds)
-    probas = np.random.multinomial(1, preds, 1)
-    return probas[0]
-
-
-def softmax2(X, tau = 1.0, axis = None):
-    """
-    Compute the softmax of each element along an axis of X.
-
-    Parameters
-    ----------
-    X: ND-Array. Probably should be floats.
-    theta (optional): float parameter, used as a multiplier
-        prior to exponentiation. Default = 1.0
-    axis (optional): axis to compute values along. Default is the
-        first non-singleton axis.
-
-    Returns an array the same size as X. The result will sum to 1
-    along the specified axis.
-    """
-
-    # make X at least 2d
-    y = np.atleast_2d(X)
-
-    # find axis
-    if axis is None:
-        axis = next(j[0] for j in enumerate(y.shape) if j[1] > 1)
-
-    # multiply y against the theta parameter,
-    y = y * float(tau)
-
-    # subtract the max for numerical stability
-    y = y - np.expand_dims(np.max(y, axis = axis), axis)
-
-    # exponentiate y
-    y = np.exp(y)
-
-    # take the sum along the specified axis
-    ax_sum = np.expand_dims(np.sum(y, axis = axis), axis)
-
-    # finally: divide elementwise
-    p = y / ax_sum
-
-    # flatten if X was 1D
-    if len(X.shape) == 1: p = p.flatten()
-
-    return p
+def soft_max(x, tau):
+    """Compute softmax values for each sets of scores in x."""
+    e_x = np.exp(np.divide(np.array(x), tau))
+    r = np.divide(e_x, e_x.sum(axis=0))
+    if np.isnan(r).any():
+        xx = x - np.max(x)
+        e_xx = np.exp(np.divide(np.array(xx), tau))
+        r = np.divide(e_xx, e_xx.sum(axis=0))
+    return r
